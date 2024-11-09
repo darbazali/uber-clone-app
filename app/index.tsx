1566b12a64659b0e0e4c578b234313c67dfdf084
Interceptor <darbaz.me@gmail.com>
2024-11-09 22:20:24 +0300

    Add 'Sign Up' button with alert functionality to Home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native"
+import { Text, TouchableOpacity, View } from "react-native"
 
 const Home = () => {
+  const handleSignUp = () => {
+    alert("Sign Up")
+  }
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-3xl color-red">Home</Text>
+      <TouchableOpacity className="mt-4" onPress={handleSignUp}>
+        <Text className="font-bold color-blue-400">Sign Up</Text>
+      </TouchableOpacity>
     </View>
   )
 }
